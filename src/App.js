c4aba4a38142bb6c0b2f5eb775a96e5d8e82830b
fedeGalvan <trosonpvp@gmail.com>
2020-10-04 22:28:10 -0300

    Header removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Fragment } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
 import Landing from "./components/Landing";
 
 function App() {
    return (
       <Fragment>
-         <Header />
          <Landing />
       </Fragment>
    );
